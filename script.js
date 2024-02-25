7bde9a54be730c378b6b9b9af048de9634ff72b3
Mario R. Gamboa-Diaz <32799750+MRGD13@users.noreply.github.com>
2024-02-25 00:05:55 -0500

    Update script.js
    Added win location, winGame function and easter egg location

--- script.js
+++ script.js
@@ -75,7 +75,20 @@ const locations = [
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: "You die. &#x2620;"
+  },
+  {
+  name: "win", 
+  "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
+  "button functions": [restart, restart, restart], 
+  text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
+  },
+  {
+  name: "easter egg",
+  "button text": ["2", "8", "Go to town square?"],
+  "button functions": [pickTwo, pickEight, goTown],
+  text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
   }
+
 
 ];
 
@@ -205,6 +218,9 @@ function goTown() {
   function lose() {
     update(locations[5]);
   }
+  function winGame() {
+    update(locations[6]);
+  }
   
   function restart() {
     xp = 0;
@@ -217,4 +233,3 @@ function goTown() {
     xpText.innerText = xp;
     goTown();
   }
-  
